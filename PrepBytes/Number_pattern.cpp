4b4f775e3a9b8ffc2b33efe18ecdb2c8ebd17f81
PRANAV VERMA <61836428+programmer-verma3827@users.noreply.github.com>
2021-09-25 11:18:39 +0530

    First commit
    First commit

---
+++ PrepBytes/Number_pattern.cpp
@@ -0,0 +1,53 @@
+/*  
+Number Pattern
+Print in the output the exact pattern provided below
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+Input format
+A single integer 
+N
+
+Output format
+Print the same pattern provided in the problem statement
+
+Constraints
+N
+=
+1
+
+Input
+1
+
+Output
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i, j, rows, last;
+    cin>>rows;
+    for(i=1;i<=5;i++)
+    {
+        last = 5-i+1;
+        for(j=1;j<=last;j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"\n\n";
+    return 0;
+}
