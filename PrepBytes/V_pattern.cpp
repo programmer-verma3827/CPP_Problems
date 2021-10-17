62252be57a2e25d7a85b977e007acd3e9a5dfd41
PRANAV VERMA <61836428+programmer-verma3827@users.noreply.github.com>
2021-10-17 11:30:57 +0530

    First commit
    First commit

---
+++ PrepBytes/V_pattern.cpp
@@ -0,0 +1,85 @@
+/*
+
+V Pattern
+Print in the output the exact pattern provided below
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+
+Input format
+A single character denoted by 
+c
+h
+
+Output format
+Print the same pattern provided in the problem statement.
+
+Constraints
+c
+h
+=
+′
+V
+′
+
+Time Limit
+ 1 second 
+
+Input
+V
+
+Output
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+
+*/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  
+  char ch;
+  cin>>ch;
+    int i = 1;
+    while(i<=)
+    {
+        int j = 1;
+        while(j<=i)
+        {
+            cout<<j;
+            j++;
+        }
+        int space = 1;
+        while(space<=n-i)
+        {
+            cout<<" ";
+            space++;
+        }
+        int spaces = 1;
+        while(spaces<=n-i)
+        {
+            cout<<" ";
+            spaces++;
+        }
+            int k = 1;
+            while(k<=i)
+            {
+                cout<<k;
+                k++;
+            }
+            cout<<endl;
+            i++;
+        }
+  return 0;
+}
