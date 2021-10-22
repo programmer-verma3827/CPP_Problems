83c2337be282a9b514430ea8d09a87bace5ec87d
PRANAV VERMA <61836428+programmer-verma3827@users.noreply.github.com>
2021-10-22 23:44:44 +0530

    First commit
    First commit

---
+++ Coding Ninjas/Patterns 2/Star_pattern.cpp
@@ -0,0 +1,64 @@
+/*
+Code : Star Pattern
+
+Print the following pattern
+Pattern for N = 4
+
+
+
+The dots represent spaces.
+
+
+
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+Constraints :
+0 <= N <= 50
+Sample Input 1 :
+3
+Sample Output 1 :
+   *
+  *** 
+ *****
+Sample Input 2 :
+4
+Sample Output 2 :
+    *
+   *** 
+  *****
+ *******
+
+*/
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    
+   int n, s, i, j;
+cin >> n;
+for(i = 1; i <= n; i++)
+{
+//for loop for displaying space
+for(s = i; s < n; s++)
+{
+cout << " ";
+}
+//for loop to display star equal to row number
+for(j = 1; j <= (2 * i - 1); j++)
+{
+cout << "*";
+}
+// ending line after each row
+cout << "\n";
+}
+    
+  
+}
