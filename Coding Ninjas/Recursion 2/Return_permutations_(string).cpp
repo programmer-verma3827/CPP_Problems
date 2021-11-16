6300bb99af1b87fe8203a3accc4c1dbda85806a5
PRANAV VERMA <61836428+programmer-verma3827@users.noreply.github.com>
2021-11-16 12:36:38 +0530

    First commit
    First commit

---
+++ Coding Ninjas/Recursion 2/Return_permutations_(string).cpp
@@ -0,0 +1,57 @@
+/*
+Return Permutations - String
+
+Given a string S, find and return all the possible permutations of the input string.
+Note 1 : The order of permutations is not important.
+Note 2 : If original string contains duplicate characters, permutations will also be duplicates.
+Input Format :
+String S
+Output Format :
+All permutations (in different lines)
+Sample Input :
+abc
+Sample Output :
+abc
+acb
+bac
+bca
+cab
+cba
+
+*/
+
+#include <iostream>
+#include <string>
+#include "solution.h"
+using namespace std;
+
+int returnPermutations(string input, string output[]){
+   	
+ if(input.size()==0){
+     output[0]==" ";
+     return 1;
+ }   
+    string smalloutput[1000];
+    int ans=returnPermutations(input.substr(1),smalloutput);
+    int k=0;
+    for(int i=0;i<ans;i++){
+        for (int j=0;j<= smalloutput[i].length();j++){
+          
+            output[k++]=smalloutput[i].substr(0,j)+input[0]+smalloutput[i].substr(j);
+        }
+    }
+    
+    return k;
+}
+
+
+int main(){
+    string input;
+    cin >> input;
+    string output[10000];
+    int count = returnPermutations(input, output);
+    for(int i = 0; i < count && i < 10000; i++){
+        cout << output[i] << endl;
+    }
+    return 0;
+}
