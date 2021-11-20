cc36801c5db19d1fde6c9c543ec4f87e789e8ed7
PRANAV VERMA <61836428+programmer-verma3827@users.noreply.github.com>
2021-11-20 12:35:07 +0530

    First commit
    First commit

---
+++ Coding Ninjas/Linked List 1/Delete_node.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+Node *deleteNode(Node *head, int pos)
+{
+    // Write your code here.
+    
+    Node* temp = head;
+    Node* prev = NULL;
+    int count=0;
+    
+    if(head == NULL)
+    {
+        return head;
+    }
+    
+    if(pos==0)
+    {
+        head = temp -> next;
+        delete(temp);
+    }
+    else
+    {
+        while(pos != count && temp != NULL)
+        {
+            ++count;
+            prev = temp;
+            temp = temp -> next;
+        }
+        if(temp != NULL)
+        {
+            prev -> next = temp -> next;
+            delete(temp);
+        }
+        if(prev -> next == NULL)
+        {
+            return head;
+        }
+    }
+    return head;
+    
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		head = deleteNode(head, pos);
+		print(head);
+	}
+
+	return 0;
+}
