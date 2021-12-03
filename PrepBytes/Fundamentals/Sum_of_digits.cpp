d28759d23ae9c44be9e3dea4bdb37577281e403c
PRANAV VERMA <61836428+programmer-verma3827@users.noreply.github.com>
2021-12-03 10:54:04 +0530

    First commit
    First commit

---
+++ PrepBytes/Fundamentals/Sum_of_digits.cpp
@@ -0,0 +1,71 @@
+/*
+
+Sum of Digits
+PrepBuddy gave you a number 
+X
+ and asks you to find the sum of the digits present in the number.
+
+Input format
+One integer is provided denoting the value of 
+X
+.
+
+Output format
+Print the sum of digits of 
+X
+.
+
+Constraints
+1
+<=
+X
+<=
+10
+6
+
+Time Limit
+1
+ second
+
+Example
+Input
+102345
+
+Output
+15
+
+Sample test case explanation
+Sum of digit 
+=
+1
++
+0
++
+2
++
+3
++
+4
++
+5
+=
+15
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int x, sum=0, n;
+  cin>>x;
+  while(x>0)
+  {
+    n = x%10;
+    sum = sum+n;
+    x=x/10;
+  }
+  cout<<sum;
+  return 0;
+}
