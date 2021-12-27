85f33dbd77c85420d6326eb79b098f5835da405c
PRANAV VERMA <61836428+programmer-verma3827@users.noreply.github.com>
2021-12-27 23:49:39 +0530

    First commit
    First commit

--- Coding Ninjas/BST/Print_elements_in_range.cpp
+++ Coding Ninjas/BST/Print_elements_in_range.cpp
@@ -42,24 +42,50 @@ void elementsInRangeK1K2(BinaryTreeNode<int>* root, int k1, int k2) {
     
 	// Write your code here
     
+	// 1st Method
+	
+//     if(root == NULL)
+//     {
+//         return;
+//     }
+    
+//     if(root->data>k1)
+//     {
+//         elementsInRangeK1K2(root->left, k1, k2);
+//     }
+    
+//     if(root->data>=k1 && root->data<=k2)
+//     {
+//         cout<<root->data<<" ";
+//     }
+    
+//     if(root->data<k2)
+//     {
+//         elementsInRangeK1K2(root->right, k1, k2);
+//     }
+	
+	// 2nd Method
+	
     if(root == NULL)
     {
         return;
     }
     
-    if(root->data>k1)
+    if(root->data >= k1 && root->data <= k2)
     {
-        elementsInRangeK1K2(root->left, k1, k2);
-    }
-    
-    if(root->data>=k1 && root->data<=k2)
-    {
+        ElementsInTheRangeBST(root->left, k1, k2);
         cout<<root->data<<" ";
+        ElementsInTheRangeBST(root->right, k1, k2);
     }
     
-    if(root->data<k2)
+    else if(root->data > k1)
     {
-        elementsInRangeK1K2(root->right, k1, k2);
+        ElementsInTheRangeBST(root->left, k1, k2);
+    }
+    
+    else if(root->data < k2)
+    {
+        ElementsInTheRangeBST(root->right, k1, k2);
     }
     
 }
